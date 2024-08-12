5f5282a9b39685a93dafe1466e553bd757bffd22
Sakshi Wanode <110322000+Sakshiwanode@users.noreply.github.com>
2024-08-12 05:28:08 +0000

    container config done

---
+++ basic_javascript/variables.js
@@ -0,0 +1,9 @@
+const accountId = 23452
+let accountEmail = "sakshi@google.com"
+var accountPassword ="12344"
+accountCity = "Bhopal"
+//accountId=2
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*prefer not touse var because of issue n block scope and functional scope  */
