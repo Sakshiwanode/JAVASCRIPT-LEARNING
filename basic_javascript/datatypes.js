17cb96a84733f57b241c9fadd5b6ed36d9d3a354
Sakshi Wanode <110322000+Sakshiwanode@users.noreply.github.com>
2024-08-12 06:11:43 +0000

    datatypes

---
+++ basic_javascript/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3);
+let name = "sakshi"
+let age = 18
+let isLoggedIn = false 
+let state = null 
+
+
+//number => 2 to power 53
+//bigint 
+//string =>""
+//boolean => true/false
+//null => standalone value 
+//undefined =>
+//symbol=> unique
+//oject 
+
+console.log(typeof"sakshi");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+ 
